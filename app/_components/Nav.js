f67db4365eb9e438a0593c6939176cf6d75a5fc8
Fin <finstars@proton.me>
2024-08-03 07:35:05 +0300

    ui/ux

--- app/_components/Nav.js
+++ app/_components/Nav.js
@@ -8,19 +8,22 @@ import { usePathname } from "next/navigation";
 const Nav = () => {
     const pathname = usePathname()
 
-    console.log(pathname)
+    const handleClick = () => {
+        document.body.classList.remove("hamburger");
+    }
+
     return (
         <div className={`${styles.nav} nav`}>
             <div className={styles.navLinks}>
-                <Link className={pathname === "/" ? styles.active : ""} href="/">
+                <Link onClick={handleClick} className={pathname === "/" ? styles.active : ""} href="/">
                     <svg xmlns="http://www.w3.org/2000/svg" version="1.1" x="0px" y="0px" viewBox="0 0 100 125" enableBackground="new 0 0 100 100" xmlSpace="preserve"><g><path fill="#fff" d="M94.485,41.471L60.371,7.363c-2.531-2.547-5.906-3.944-9.513-3.944h-0.303c-3.589,0-6.957,1.39-9.489,3.922   L6.944,41.465C4.398,44.007,3,47.383,3,50.977c0,3.596,1.398,6.973,3.92,9.484c2.54,2.547,5.919,3.944,9.513,3.944   c0.092,0,0.187-0.005,0.283-0.005v18.786c0,7.037,5.708,12.745,12.746,12.745h42.493c7.038,0,12.746-5.708,12.746-12.745V64.4   c0.096,0,0.188,0.005,0.286,0.005c3.591,0,6.966-1.391,9.498-3.935C99.727,55.236,99.727,46.712,94.485,41.471z M58.673,80.301   c0,3.938-3.566,7.134-7.969,7.134c-4.4,0-7.965-3.195-7.965-7.134v-8.497c0-3.938,3.564-7.134,7.965-7.134   c4.402,0,7.969,3.195,7.969,7.134V80.301z" /></g></svg>
                 </Link>
 
-                <Link className={pathname.includes("/shadowing-technique-english-lessons") ? styles.active : ""} href="/shadowing-technique-english-lessons"><span>Lvl 1. Beginner</span> Shadowing</Link>
-                <Link className={pathname.includes("/conversational-english-lessons") ? styles.active : ""} href="/conversational-english-lessons"><span>Lvl 2. Intermediate</span> Conversational</Link>
-                <Link className={pathname.includes("/tpr-storytelling-english-lessons") ? styles.active : ""} href="/tpr-storytelling-english-lessons"><span>Lvl 3. Advanced</span> TPR Storytelling</Link>
+                <Link onClick={handleClick} className={pathname.includes("/shadowing-technique-english-lessons") ? styles.active : ""} href="/shadowing-technique-english-lessons"><span>Lvl 1. Beginner</span> Shadowing</Link>
+                <Link onClick={handleClick} className={pathname.includes("/conversational-english-lessons") ? styles.active : ""} href="/conversational-english-lessons"><span>Lvl 2. Intermediate</span> Conversational</Link>
+                <Link onClick={handleClick} className={pathname.includes("/tpr-storytelling-english-lessons") ? styles.active : ""} href="/tpr-storytelling-english-lessons"><span>Lvl 3. Advanced</span> TPR Storytelling</Link>
 
-                <Link href="https://shop.englishflow.ai">
+                <Link onClick={handleClick} href="https://shop.englishflow.ai">
                     Shop
                     <svg fill="#fff" height="800px" width="800px" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 194.818 194.818" xmlSpace="preserve">
                         <g>
