905c475baa359ba4dae0666ec8dd69f026a746b5
Fin <finstars@proton.me>
2024-11-25 11:35:04 +0200

    new structure

--- app/_components/Nav.js
+++ app/_components/Nav.js
@@ -19,9 +19,13 @@ const Nav = () => {
                     <svg xmlns="http://www.w3.org/2000/svg" version="1.1" x="0px" y="0px" viewBox="0 0 100 125" enableBackground="new 0 0 100 100" xmlSpace="preserve"><g><path fill="#fff" d="M94.485,41.471L60.371,7.363c-2.531-2.547-5.906-3.944-9.513-3.944h-0.303c-3.589,0-6.957,1.39-9.489,3.922   L6.944,41.465C4.398,44.007,3,47.383,3,50.977c0,3.596,1.398,6.973,3.92,9.484c2.54,2.547,5.919,3.944,9.513,3.944   c0.092,0,0.187-0.005,0.283-0.005v18.786c0,7.037,5.708,12.745,12.746,12.745h42.493c7.038,0,12.746-5.708,12.746-12.745V64.4   c0.096,0,0.188,0.005,0.286,0.005c3.591,0,6.966-1.391,9.498-3.935C99.727,55.236,99.727,46.712,94.485,41.471z M58.673,80.301   c0,3.938-3.566,7.134-7.969,7.134c-4.4,0-7.965-3.195-7.965-7.134v-8.497c0-3.938,3.564-7.134,7.965-7.134   c4.402,0,7.969,3.195,7.969,7.134V80.301z" /></g></svg>
                 </Link>
 
-                <Link onClick={handleClick} className={pathname.includes("/shadowing-technique-english-lessons") ? styles.active : ""} href="/shadowing-technique-english-lessons"><span>80% of English</span> 1-Week Course</Link>
+                <Link onClick={handleClick} className={pathname.includes("/shadowing-technique-english-lessons") ? styles.active : ""} href="/shadowing-technique-english-lessons"><span>Video Lessons</span> 1-Week Course</Link>
 
-                <Link onClick={handleClick} className={pathname.includes("/shadowing-technique-english-lessons") ? styles.active : ""} href="/shadowing-technique-english-lessons"><span>Beginner to Advanced</span> Practice Lessons</Link>
+                <Link onClick={handleClick} className={pathname.includes("/shadowing-technique-english-lessons") ? styles.active : ""} href="/shadowing-technique-english-lessons"><span>Self-Paced</span> English Practice</Link>
+                
+                <Link onClick={handleClick} className={pathname.includes("/shadowing-technique-english-lessons") ? styles.active : ""} href="/shadowing-technique-english-lessons"><span>Blog</span> Language Learning</Link>
+
+                <Link onClick={handleClick} className={pathname.includes("/shadowing-technique-english-lessons") ? styles.active : ""} href="/shadowing-technique-english-lessons"><span>About us</span> Team & Mission</Link>
 
                 {/* <Link onClick={handleClick} className={pathname.includes("/shadowing-technique-english-lessons") ? styles.active : ""} href="/shadowing-technique-english-lessons"><span>Lvl 1. Beginner</span> Shadowing</Link>
                 <Link onClick={handleClick} className={pathname.includes("/conversational-english-lessons") ? styles.active : ""} href="/conversational-english-lessons"><span>Lvl 2. Intermediate</span> Conversational</Link>
